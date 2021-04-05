394b109c6f8748c6ff1d44a13603c2fdf6d995bb
markoscapin <59868532+markoscapin@users.noreply.github.com>
2021-04-05 21:14:42 +0200

    add server, db, ejs template
    Add Server with express, add db with mongoose, now it store the data on local storage and display it with ejs. Provide ejs layout, add some css. Order automatically depending on index of the task

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//REQUIRE NPM
 const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
@@ -5,30 +6,71 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+//SETUP AND USE 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
 
-const items = []
+
+//MONGOOSE SECTION
+mongoose.connect('mongodb://localhost:27017/todoAppDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const tasksSchema = {
+    name: String,
+    index: Number
+}
+
+const Task = mongoose.model("Task", tasksSchema);
 
 
+
+
+//APP --> GET
 app.get("/", function(req, res) {
-    res.render('index', {itemsArray: items})
+    Task.find({}, function(err, taskArray) {
+
+        //This func allow us to order the tasks with index parameter
+        function compareNumbers(a, b) {
+            return a.index - b.index;
+          }
+        //.sort() return an orderedArray
+        taskArray = taskArray.sort(compareNumbers);
+
+        (err) ? console.log(err) : res.render('index', {taskArray: taskArray})
+        });
 });
 
-app.post("/", function(req, res) {
-    const newItem = req.body.newItem;
-    const checked = req.body.checkbox;
 
-    items.push(newItem);
+//APP --> POST
+app.post("/", function(req, res) {
+    const newTaskToAdd = req.body.newItem;
+
+    Task.find({}, function(err, taskArray) {
+        const newTask = new Task({name:newTaskToAdd, index: taskArray.length});
+        newTask.save();
+    });
+
     res.redirect("/");
+
+})
+
+app.post("/delete", function(req, res) {
+    const taskToDeleteID = req.body.checkbox;
+    
+    //this delete the selected item by id
+    Task.deleteOne({_id: taskToDeleteID}, function(err){
+        (err) ? console.log(err) : 
+        console.log("Deleted");
+
+        res.redirect("/");
+    })
 
 })
 
 
 
 
-
+//APP --> LISTEN
 app.listen(3000, function() {
     console.log("Server running on port 3000")
 });
